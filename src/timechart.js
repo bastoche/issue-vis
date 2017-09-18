d0341b75422d13a4ba2cb7c391f85ccff7530c12
Bastien Duret <bastien.duret@gmail.com>
2017-09-18 12:17:57 +0200

    Add timechart

---
+++ src/timechart.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  FlexibleXYPlot,
+  HorizontalGridLines,
+  LineSeries,
+  VerticalGridLines,
+  XAxis,
+  YAxis,
+} from 'react-vis';
+
+export class TimeChart extends Component {
+  render() {
+    const data = this.props.data;
+    return (
+      <FlexibleXYPlot height={300} xType="time">
+        <VerticalGridLines />
+        <HorizontalGridLines />
+        <XAxis />
+        <YAxis />
+        <LineSeries data={data} />
+      </FlexibleXYPlot>
+    );
+  }
+}
+
+TimeChart.propTypes = {
+  data: PropTypes.array,
+};
