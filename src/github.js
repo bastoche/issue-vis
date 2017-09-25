c43d665de704cc35aca858ffbb15f146a429ca5e
Bastien Duret <bastien.duret@gmail.com>
2017-09-25 09:39:09 +0200

    Add since date for issues

--- src/github.js
+++ src/github.js
@@ -38,5 +38,5 @@ export function getNextPage(linkHeader) {
 }
 
 export function issuesUrl(repository) {
-  return `https://api.github.com/repos/${repository}/issues?state=all`;
+  return `https://api.github.com/repos/${repository}/issues?state=all&since=2017-08-01T00:00:00Z`;
 }
